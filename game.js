f09efcd970e659bfc694e832a7e62f994c3036da
Berkay Alkan <berky.alknn@gmail.com>
2024-09-28 02:34:30 +0300

    add restart option for game

--- game.js
+++ game.js
@@ -2,8 +2,17 @@
 
 let gamePattern = [];
 let buttonColours = ['red', 'blue', 'green', 'yellow'];
-
 let userClickedPattern = [];
+let started = false;
+let level = 0;
+
+$(document).keydown(function () {
+    if(!started) {
+        $("#level-title").text("Level " + level);
+        nextSequence();
+        started = true;
+    }
+});
 
 $(".btn").click(function () {
 
@@ -12,12 +21,49 @@ $(".btn").click(function () {
 
     playSound(userChosenColour);
     animatePress(userChosenColour);
+
+    checkAnswer(userClickedPattern.length -1);
    
 });
 
+function checkAnswer(currentLevel) {
+
+    if(gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+
+        console.log("success");
+    
+
+    if(userClickedPattern.length === gamePattern.length) {
+                setTimeout(function (){
+                nextSequence();
+            }, 1000)
+        }
+    } else {
+
+        console.log("wrong");
+
+        playSound("wrong");
+
+        $("body").addClass("game-over");
+
+        setTimeout(function () {
+            $("body").removeClass("game-over");
+        }, 200);
+
+        $("#level-title").text("Game Over, Press Any Key to Restart");
+
+        startOver();
+    }
+}
+
 function nextSequence() {
     
-    let randomNumber = Math.round(Math.random * 3);
+    userClickedPattern = [];
+    level ++;
+
+    $("#level-title").text("Level " + level);
+
+    let randomNumber = Math.floor(Math.random() * 4);
     let randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour)
 
@@ -39,5 +85,12 @@ function animatePress(currentColour) {
 
     setTimeout(function() {
     $("#" + currentColour).removeClass("pressed")}, 100);
+}
+
+function startOver() {
+
+    level = 0;
+  gamePattern = [];
+  started = false;
 }
 
